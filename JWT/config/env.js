a3349a70da06338cdaa2fc1610fcc118fd73cbd3
alisonmiranda <asmiranda@espe.edu.ec>
2026-06-20 01:49:20 -0500

    chore: configure sentry environment

--- JWT/config/env.js
+++ JWT/config/env.js
@@ -8,5 +8,7 @@ export const config = {
     JWT_SECRET: process.env.JWT_SECRET,
     PRIVATE_KEY: process.env.PRIVATE_KEY_PATH ? fs.readFileSync(process.env.PRIVATE_KEY_PATH, 'utf8') : null,
     PUBLIC_KEY: process.env.PUBLIC_KEY_PATH ? fs.readFileSync(process.env.PUBLIC_KEY_PATH, 'utf8') : null,
-    ALGORITHM: process.env.JWT_ALGORITHM || 'HS256' // 'HS256' o 'RS256'
+    ALGORITHM: process.env.JWT_ALGORITHM || 'HS256', // 'HS256' o 'RS256'
+    SENTRY_DSN: process.env.SENTRY_DSN || '',
+    SENTRY_ENVIRONMENT: process.env.SENTRY_ENVIRONMENT || 'development'
 };
